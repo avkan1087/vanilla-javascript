b820e69f588dc5d54663ece013ecec8342a1149a
avi <avkan1087@gmail.com>
2020-06-24 22:43:37 -0400

    sort method

--- array-methods/script.js
+++ array-methods/script.js
@@ -33,6 +33,13 @@ function doubleMoney() {
    updateDOM();
 }
 
+// sort by richest
+function sortByRichest() {
+   data.sort((a, b) => b.money - a.money);
+
+   updateDOM();
+}
+
 //add new obj to data arr
 function addData(obj) {
    data.push(obj);
@@ -64,3 +71,4 @@ function formatMoney(number) {
 
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
