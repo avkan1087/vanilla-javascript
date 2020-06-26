066a5bf49e53c4f7d34948131cfabd531b948001
avi <avkan1087@gmail.com>
2020-06-26 12:39:41 -0400

    reduce method

--- array-methods/script.js
+++ array-methods/script.js
@@ -47,6 +47,18 @@ function showMillionaires() {
    updateDOM();
 }
 
+// calculate total wealth
+function calculateWealth() {
+   const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+   const wealthEl = document.createElement("div");
+   wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+      wealth
+   )}</strong></h3>`;
+
+   main.appendChild(wealthEl);
+}
+
 //add new obj to data arr
 function addData(obj) {
    data.push(obj);
@@ -80,3 +92,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
